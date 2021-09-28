0cfe4148b656000e5d8cb96f4c72b757d8de5313
Leonid Shestera <leonid@shestera.com>
2021-09-28 12:38:05 +0300

    Change author name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='IRR',
       version='0.1',
       description='numpy.irr but works',
-      author='Erik Bernhardsson',
-      author_email='mail@erikbern.com',
+      author='Leonid Shestera',
+      author_email='leonid@shestera.com',
       packages=['irr'],
 )
